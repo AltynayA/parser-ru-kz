c0ef972712ccdeab1f75c2a6a65bd8a5213bbbab
altynay <goldmoon321123@gmail.com>
2025-12-23 12:43:07 +0500

    Save the response to txt and json

--- app/tasks/ocr_task.py
+++ app/tasks/ocr_task.py
@@ -5,8 +5,18 @@ from app.storage.memory import update_result
 from app.ocr.pdf_converter import pdf_to_images
 from app.ocr.ocr_engine import image_to_text
 
+from pathlib import Path
+import json
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("OCR")
+
+RESULT_JSON_DIR = Path("/app/storage/logs")
+RESULT_TEXT_DIR = Path("/app/storage/texts")
+RESULT_JSON_DIR.mkdir(parents=True, exist_ok=True)
+RESULT_TEXT_DIR.mkdir(parents=True, exist_ok=True)
+
+
 
 def run_ocr_task(result_id: str, pdf_path: str):
     start_time = time.time()
@@ -17,7 +27,6 @@ def run_ocr_task(result_id: str, pdf_path: str):
         for idx, img in enumerate(images):
             text = image_to_text(img)
             full_text += text + "\n"
-
         duration = time.time() - start_time
 
         # Update result in memory
@@ -26,7 +35,18 @@ def run_ocr_task(result_id: str, pdf_path: str):
             "full_text": full_text.strip(),
             "duration_seconds": round(duration, 3)
         })
-
+        json_path = RESULT_JSON_DIR / f"{result_id}.json"
+        with open(json_path, "w", encoding="utf-8") as f:
+            json.dump({
+                "result_id": result_id,
+                "status": "completed",
+                "full_text": full_text.strip(),
+                "duration_seconds": round(duration, 3)
+            }, f, ensure_ascii=False, indent=2)
+            
+        text_path = RESULT_TEXT_DIR / f"{result_id}.txt"
+        with open(text_path, "w", encoding="utf-8") as f:
+            f.write(full_text.strip())
         logger.info(f"[OCR] Completed for result_id={result_id} in {round(duration, 3)}s")
 
     except Exception as e:
